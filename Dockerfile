897ed1cbeee377c87ed4965d831298b1bc041b11
antonkulaga <antonkulaga@gmail.com>
2017-07-16 10:31:05 +0300

    updated hue

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,6 @@
-FROM earthquakesan/hue-build-env:branch-3.11
-MAINTAINER Ivan Ermilov <ivan.s.ermilov@gmail.com>
-
-RUN mkdir -p /opt/hue
-WORKDIR /opt/hue
-
-RUN git clone https://github.com/cloudera/hue.git ./
-RUN git checkout branch-3.11
-RUN make apps
-
+FROM gethue/hue:latest
 ADD entrypoint.sh /entrypoint.sh
 RUN chmod a+x /entrypoint.sh
-
-COPY common_header.mako /opt/hue/desktop/core/src/desktop/templates/common_header.mako
 
 EXPOSE 8088
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-HUE_HOME="/opt/hue/"
+HUE_HOME="/hue"
 HUE_CONFIG_FILE="$HUE_HOME/desktop/conf/pseudo-distributed.ini"
-APP_BLACKLIST="search,security,oozie,jobbrowser,pig,beeswax,search,zookeeper,impala,rdbms,spark,metastore,hbase,sqoop,jobsub,about"
+APP_BLACKLIST="security,jobbrowser,pig,zookeeper,impala,rdbms,spark,metastore,hbase,sqoop,jobsub"
 
-sed -i "s#.*blacklist.*#  app_blacklist=$APP_BLACKLIST#g" $HUE_CONFIG_FILE
+#sed -i "s#.*blacklist.*#  app_blacklist=$APP_BLACKLIST#g" $HUE_CONFIG_FILE
 sed -i "s#.*webhdfs_url.*#  webhdfs_url=http://$NAMENODE_HOST:50070/webhdfs/v1#g" $HUE_CONFIG_FILE
 
 exec $@
